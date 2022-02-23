91561e34ae516616c5629cf1cf5f81a65f4b0a7a
arunks <arun.jnpr@gmail.com>
2022-02-23 10:47:47 +0530

    new files

---
+++ generator_example.py
@@ -0,0 +1,12 @@
+# This is example of generator function usage
+# test
+def example():
+    i = 0
+    while i < 10:
+        yield i   # yield returns current value and remembers it when the function is called again
+        i +=1
+
+x = example()
+print(next(x)) #next calls the current value in generator function.
+print(next(x))
+print(next(x))
