808fbe1c672b2a636d4aad40c416152c44e9a97a
arunks <arun.jnpr@gmail.com>
2022-03-16 19:40:01 +0530

    new files

--- Practice_Projects/talking_dictionary.py
+++ Practice_Projects/talking_dictionary.py
@@ -4,11 +4,11 @@ from selenium.webdriver.common.keys import Keys
 import speech_recognition as sr
 import pyttsx3
 
-PATH = "/Users/arunks/Desktop/Python_Studies/chromedriver" # Define chrome driver location
+PATH = "/chromedriver"  # Define chrome driver location
 driver = webdriver.Chrome(PATH)  # Define object named driver
 listener = sr.Recognizer()
 speaker = pyttsx3.init()
-speaker.setProperty('rate' , 150)
+speaker.setProperty('rate' , 160)
 #voices = speaker.getProperty('voices')
 #speaker.setProperty('voice' , voices[1].id)
 
@@ -27,15 +27,19 @@ def take_command():
 
 #word = input("Find meaning of : ")
 def get_result(word):
-    #driver.get('https://quotes.toscrape.com/')  # Open the website
-    driver.get('https://google.com/')
-    locator = 'input.gLFyf gsfi'
-    search = driver.find_element(By.CSS_SELECTOR,'div.L3eUgb input.gLFyf.gsfi')
-    search.send_keys(f'{word} meaning')
-    search.send_keys(Keys.RETURN)
-    meaning = driver.find_element(By.CSS_SELECTOR,'div.LTKOO.sY7ric').text
-    print(f"**********************Meaning of {word} ************************")
-    print(meaning)
+    try:
+        #driver.get('https://quotes.toscrape.com/')  # Open the website
+        driver.get('https://google.com/')
+        locator = 'input.gLFyf gsfi'
+        search = driver.find_element(By.CSS_SELECTOR,'div.L3eUgb input.gLFyf.gsfi')
+        search.send_keys(f'{word} meaning')
+        search.send_keys(Keys.RETURN)
+        meaning = driver.find_element(By.CSS_SELECTOR,'div.LTKOO.sY7ric').text
+        print(f"**********************Meaning of {word} ************************")
+        print(meaning)
+    except:
+        meaning = 'Sorry , I couldnt find it for you'
+        print(meaning)
     return meaning
 
 def read_result(meaning):
